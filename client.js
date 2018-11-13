17cc98c75977527e9ebd816ebfb5df479ce27914
Vang Xiong <vxiong029@gmail.com>
2018-11-13 13:00:08 -0600

    completed assignment

--- client.js
+++ client.js
@@ -4,25 +4,34 @@ let clickCount = 0;
 function readyNow() {
   $('#generateButton').on('click', generateButton);
   $('div').on('click', '.swapColor', toggleColor);
+  $('div').on('click', '.deleteButton', deleteButton);
 }
 
 function generateButton() {
   console.log('clicked on generate button');
   clickCount++;
-  $('#content').append(`
-    <div class="box"> 
+  $('#content').append(
+    `<div class="box red" data-name="changeColor">
       <p>${clickCount}</p>
-      <button class="swapColor" id="${clickCount}">Swap Color</button>
-      <button id="deleteButton">Delete</button>
-    </div>
-  `);
+      <button class="swapColor">Swap Color</button>
+      <button class="deleteButton">Delete</button>
+    </div>`);
 }
 
 function toggleColor() {
-  let elementId = $(this).attr('id');
-  console.log('elementId', elementId);
-  if(clickCount == elementId) {
-    $('.box').css('background-color', 'yellow');
+  let buttonElement = $(this).closest('div');
+  
+  console.log('element', buttonElement);
+  if ('box red' == buttonElement.attr('class')) {
+     console.log('its red');
+    $(buttonElement).removeClass('red').addClass('yellow');
+  } else {
+    $(buttonElement).removeClass('yellow').addClass('red');
   }
+}
 
+function deleteButton() {
+  let deleteElement = $(this).closest('div');
+  deleteElement.remove();
+  console.log(deleteElement);
 }
